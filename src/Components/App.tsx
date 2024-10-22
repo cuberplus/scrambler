ad2f46c705b87a70a3ad4038e2780679f0e436d7
mpfeifer1 <mikepf97@gmail.com>
2024-10-22 14:23:23 -0700

    Tested using scrambler library

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import "../CSS/Style.css";
 import { Row, Col, Card, Navbar, Button } from 'react-bootstrap'
+import { ScrambleDisplay } from "scramble-display"
+
 
 function App() {
-  return (
+
+  let scramble = new ScrambleDisplay();
+  scramble.scramble = "F' D F2 L2 U' R2 U' F2 D2 R2 F2 U2 F R' B F D' B2 U";
+  scramble.visualization = "2D";
+
+
+  let page = (
     <div className="App">
       <header className={"header"}>
         <Navbar>
@@ -14,18 +22,6 @@ function App() {
       </header>
       <Row style={{ margin: 10 }}>
         <Col className='col-lg-6'>
-          <Card className='p-3'>
-            <a href='/cubeastanalyzer' className='stretched-link'>
-              <h3>
-                Cubeast Analyzer
-              </h3>
-            </a>
-            <img
-              src={require("../Assets/CubeastAnalyzer.png")}
-              style={{ border: '1px solid #000000' }}
-            >
-            </img>
-          </Card>
         </Col>
 
         <Col className='col-lg-6'>
@@ -40,7 +36,11 @@ function App() {
       </Row>
 
     </div>
-  );
+  )
+
+  document.body.appendChild(scramble);
+
+  return page;
 }
 
 export default App;
